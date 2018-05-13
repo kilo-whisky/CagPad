bac470b92b34d79bb95d372dd794a87da588fb88
Kyle <steel.woodrow@gmail.com>
2018-05-14 00:44:07 +0100

    creating controllers

---
+++ GuardianChecks/GuardianChecks/Controllers/PADController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GuardianChecks.Controllers
+{
+	public class PADController : Controller
+	{
+		// GET: PAD
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
