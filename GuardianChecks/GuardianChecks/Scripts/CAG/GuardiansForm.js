1d27c79558a99bcf5ac70cab638f35b63c36cb3c
Kyle <steel.woodrow@gmail.com>
2018-06-06 01:09:13 +0100

    guardian form

---
+++ GuardianChecks/GuardianChecks/Scripts/CAG/GuardiansForm.js
@@ -0,0 +1,29 @@
+﻿$('#frm-check').on('submit', function(e){
+	e.preventDefault();
+	console.log($(this).serializeArray())
+})
+
+$('.toggle').bootstrapToggle({
+	on: "Yes",
+	off: "No",
+	onstyle: "success",
+	offstyle: "danger",
+	size: "small"
+});
+
+$('.toggle').on('change', function (e) {
+	var thing = this.id.slice(4)
+	if ($(this).prop('checked') === true) {
+		$('#btn-' + thing).addClass('disabled').prop("disabled", true);
+	}
+	else {
+		$('#btn-' + thing).removeClass('disabled').prop("disabled", false);
+	}
+});
+
+$('.btn-note').on("click", function () {
+
+	$('#modal').modal('show');
+});
+
+$()
